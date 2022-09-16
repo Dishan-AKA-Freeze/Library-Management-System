204f54fea633c0d5da74459f77a967a976c16f2f
Pasindu Prabhath Manuranga <113160426+PrabhathManuranga@users.noreply.github.com>
2022-09-16 17:00:08 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,21 @@
+<?php
+
+//footer.php
+
+?>               
+				
+                <footer class="pt-3 mt-4 text-muted text-center border-top">
+                    &copy; <?php echo date('Y'); ?>
+                </footer>
+            </div>
+        </main>
+
+
+    	<script src="<?php echo base_url(); ?>asset/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="<?php echo base_url(); ?>asset/js/scripts.js"></script>
+        <script src="<?php echo base_url(); ?>asset/js/simple-datatables@latest.js" crossorigin="anonymous"></script>
+        <script src="<?php echo base_url(); ?>asset/js/datatables-simple-demo.js"></script>
+
+    </body>
+
+</html>
