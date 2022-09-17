4baced48b2c63189520d0ce36d8e43644629ab61
Nath Duminda <nathduminda@gmail.com>
2022-09-17 10:28:48 +0530

    Update function.php

--- function.php
+++ function.php
@@ -7,4 +7,22 @@ function base_url()
 	return 'http://localhost/Library-Management-System/';
 }
 
+function is_admin_login()
+{
+	if(isset($_SESSION['admin_id']))
+	{
+		return true;
+	}
+	return false;
+}
+
+function is_user_login()
+{
+	if(isset($_SESSION['user_id']))
+	{
+		return true;
+	}
+	return false;
+}
+
 ?>
