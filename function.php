367e1f30ed5baa2eeb28d3d178ac078a9ff427e0
Dishan Chalana <84405840+Dishan-AKA-Freeze@users.noreply.github.com>
2022-09-19 11:58:42 +0530

    Update function.php

--- function.php
+++ function.php
@@ -191,4 +191,212 @@ function get_total_book_issue_day($connect)
 	return $output;
 }
 
+function fill_author($connect)
+{
+	$query = "
+	SELECT author_name FROM lms_author 
+	WHERE author_status = 'Enable' 
+	ORDER BY author_name ASC
+	";
+
+	$result = $connect->query($query);
+
+	$output = '<option value="">Select Author</option>';
+
+	foreach($result as $row)
+	{
+		$output .= '<option value="'.$row["author_name"].'">'.$row["author_name"].'</option>';
+	}
+
+	return $output;
+}
+
+function fill_category($connect)
+{
+	$query = "
+	SELECT category_name FROM lms_category 
+	WHERE category_status = 'Enable' 
+	ORDER BY category_name ASC
+	";
+
+	$result = $connect->query($query);
+
+	$output = '<option value="">Select Category</option>';
+
+	foreach($result as $row)
+	{
+		$output .= '<option value="'.$row["category_name"].'">'.$row["category_name"].'</option>';
+	}
+
+	return $output;
+}
+
+function fill_location_rack($connect)
+{
+	$query = "
+	SELECT location_rack_name FROM lms_location_rack 
+	WHERE location_rack_status = 'Enable' 
+	ORDER BY location_rack_name ASC
+	";
+
+	$result = $connect->query($query);
+
+	$output = '<option value="">Select Location Rack</option>';
+
+	foreach($result as $row)
+	{
+		$output .= '<option value="'.$row["location_rack_name"].'">'.$row["location_rack_name"].'</option>';
+	}
+
+	return $output;
+}
+
+function Count_total_issue_book_number($connect)
+{
+	$total = 0;
+
+	$query = "SELECT COUNT(issue_book_id) AS Total FROM lms_issue_book";
+
+	$result = $connect->query($query);
+
+	foreach($result as $row)
+	{
+		$total = $row["Total"];
+	}
+
+	return $total;
+}
+
+function Count_total_returned_book_number($connect)
+{
+	$total = 0;
+
+	$query = "
+	SELECT COUNT(issue_book_id) AS Total FROM lms_issue_book 
+	WHERE book_issue_status = 'Return'
+	";
+
+	$result = $connect->query($query);
+
+	foreach($result as $row)
+	{
+		$total = $row["Total"];
+	}
+
+	return $total;
+}
+
+function Count_total_not_returned_book_number($connect)
+{
+	$total = 0;
+
+	$query = "
+	SELECT COUNT(issue_book_id) AS Total FROM lms_issue_book 
+	WHERE book_issue_status = 'Not Return'
+	";
+
+	$result = $connect->query($query);
+
+	foreach($result as $row)
+	{
+		$total = $row["Total"];
+	}
+
+	return $total;
+}
+
+function Count_total_fines_received($connect)
+{
+	$total = 0;
+
+	$query = "
+	SELECT SUM(book_fines) AS Total FROM lms_issue_book 
+	WHERE book_issue_status = 'Return'
+	";
+
+	$result = $connect->query($query);
+
+	foreach($result as $row)
+	{
+		$total = $row["Total"];
+	}
+
+	return $total;
+}
+
+function Count_total_book_number($connect)
+{
+	$total = 0;
+
+	$query = "
+	SELECT COUNT(book_id) AS Total FROM lms_book 
+	WHERE book_status = 'Enable'
+	";
+
+	$result = $connect->query($query);
+
+	foreach($result as $row)
+	{
+		$total = $row["Total"];
+	}
+
+	return $total;
+}
+
+function Count_total_author_number($connect)
+{
+	$total = 0;
+
+	$query = "
+	SELECT COUNT(author_id) AS Total FROM lms_author 
+	WHERE author_status = 'Enable'
+	";
+
+	$result  = $connect->query($query);
+
+	foreach($result as $row)
+	{
+		$total = $row["Total"];
+	}
+
+	return $total;
+}
+
+function Count_total_category_number($connect)
+{
+	$total = 0;
+
+	$query = "
+	SELECT COUNT(category_id) AS Total FROM lms_category 
+	WHERE category_status = 'Enable'
+	";
+
+	$result = $connect->query($query);
+
+	foreach($result as $row)
+	{
+		$total = $row["Total"];
+	}
+	return $total;
+}
+
+function Count_total_location_rack_number($connect)
+{
+	$total = 0;
+
+	$query = "
+	SELECT COUNT(location_rack_id) AS Total FROM lms_location_rack 
+	WHERE location_rack_status = 'Enable'
+	";
+
+	$result = $connect->query($query);
+
+	foreach($result as $row)
+	{
+		$total = $row["Total"];
+	}
+
+	return $total;
+}
+
 ?>
